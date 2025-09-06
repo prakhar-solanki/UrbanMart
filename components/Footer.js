8ed7d81dce6e18e8b2543af34845598b6b6024c7
prakhar-solanki <prakharsolanki2002@gmail.com>
2025-09-06 13:51:57 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center mt-8">
-      <p>© 2025 MyStore. All rights reserved.</p>
+      <p>© 2025 UrbanMart. All rights reserved.</p>
     </footer>
   );
 }
